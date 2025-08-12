109b331e3759332ea394d2237e06eac6b9f2f53f
king coombs <jaccoo53@kingshigh.school.nz>
2025-08-12 14:58:46 +1200

    made progress on tutorial and made sound

--- new-game-project/main_menu.gd
+++ new-game-project/main_menu.gd
@@ -11,3 +11,10 @@ func _ready():
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://world.tscn")
+
+
+	
+
+
+func _on_tutorial_button_pressed():
+	get_tree().change_scene_to_file("res://tutorial.tscn")
